cfb7a54a4333bf16a38a3ab1aa64bb1cd16f462e
Tsames <tsames1@gmail.com>
2025-05-15 16:10:06 -0700

    Added workout view and button to navigate from the main page to workout view. There is no button to navigate back to the main screen. [wip]

--- app/src/main/java/com/tsames/lograt/Workout.kt
+++ app/src/main/java/com/tsames/lograt/Workout.kt
@@ -1,28 +1,16 @@
 package com.tsames.lograt
 
-import android.content.Intent
-import android.os.Build.VERSION_CODES.N
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.jvm.java
 
-class MainActivity : AppCompatActivity() {
+class Workout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val workoutButton = findViewById<Button>(R.id.workoutButton)
-
-        workoutButton.setOnClickListener {
-            val intent = Intent(this, Workout::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_workout)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
